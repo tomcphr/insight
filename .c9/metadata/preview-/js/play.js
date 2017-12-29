73ece41d859910142fe60d4db257134acd7d9bda
tomcphr <tchadwick@regatta.com>
2017-12-29 07:40:03 +0000

    A bunch of changes

--- .c9/metadata/preview-/js/play.js
+++
@@ -1,1 +0,0 @@
-{"filter":false,"title":"[P] /js/play.js","undoManager":{"mark":-1,"position":-1,"stack":[]},"preview":{"path":"/index.html","previewer":"preview.browser"},"timestamp":1514480921891,"hash":"da39a3ee5e6b4b0d3255bfef95601890afd80709"}
--- js/play.js
+++ js/play.js
@@ -1,107 +1,162 @@
 window.onload = function () {
-     // Create the state that will contain the whole game
-    var playState = {  
-        preload: function() {  
-            // Here we preload the assets
-            game.load.image("player", "assets/player.png");
-            
-            game.load.image("wall", "assets/wall.png");
-            
-            game.load.image("enemy", "assets/enemy.png");
+    var playState = {
+        preload: function () {
+            game.load.image("grass", "assets/sprites/grass.png");
+            game.load.image("dirt", "assets/sprites/dirt.png");
+            game.load.image("stone", "assets/sprites/stone.png");
+            game.load.image("bedrock", "assets/sprites/bedrock.png");
+
+            game.load.image("player", "assets/sprites/player.png");
+            game.load.image("enemy", "assets/sprites/enemy.png");
+
+            game.time.advancedTiming = true;
         },
-    
-        create: function() {  
-            // Set the background color to blue
-            game.stage.backgroundColor = "#3598db";
+
+        create: function () {
+            game.stage.backgroundColor = "#87CEFA";
+
+            game.world.setBounds(0, 0, 2000, 1000);
 
             // Start the Arcade physics system (for movements and collisions)
             game.physics.startSystem(Phaser.Physics.ARCADE);
 
             // Add the physics engine to all game objects
             game.world.enableBody = true;
-            
+
             // Variable to store the arrow key pressed
             this.cursor = game.input.keyboard.createCursorKeys();
-            
+
             // Create the player in the middle of the game
-            this.player = game.add.sprite(70, 100, "player");
-            
+            this.player = game.add.sprite(game.world.centerX, game.world.centerY, "player");
+            this.player.body.collideWorldBounds = true;
+
             // Add gravity to make it fall
             this.player.body.gravity.y = 900;
-            
-            handleLevel(this);
+
+            game.camera.follow(this.player);
+
+            this.blocks = this.add.group();
+
+            handleLevel(this, game);
         },
-    
-        update: function() {  
+
+        update: function () {
             handleCollision(this, game);
-            
-            handleMovement(this);
+
+            handleMovement(this, game);
+
+            game.world.wrap(this.player, 0, true);
         },
-        
-        restart: function() {
+
+        restart: function () {
             game.state.start("play");
+        },
+
+        render: function () {
+            game.debug.text(game.time.fps || '--', 2, 14, "#00ff00");
         }
     };
-    
-    // Initialize the game and start our state
-    var game = new Phaser.Game(500, 200);  
-    
-    game.state.add("play", playState);  
-    
+
+    var width = 480;
+    var height = 320;
+
+    var game = new Phaser.Game(width, height, Phaser.CANVAS);
+
+    game.state.add("play", playState);
+
     game.state.start("play");
 }
 
-function handleMovement(phaser) {
-    phaser.player.body.velocity.x = 0;
-    
+
+function handleMovement(phaser, game)
+{
     var movementSpeed = 200;
-    var jumpingSpeed = 250;
-    
+
+    phaser.player.body.velocity.x = 0;
+
     if (phaser.cursor.left.isDown) {
         phaser.player.body.velocity.x = -movementSpeed;
     } else if (phaser.cursor.right.isDown) {
         phaser.player.body.velocity.x = movementSpeed;
     }
 
+    var jumpingSpeed = 250;
+
     // Make the player jump if he is touching the ground
-    if (phaser.cursor.up.isDown && phaser.player.body.touching.down) 
+    if (phaser.cursor.up.isDown && phaser.player.body.touching.down) {
         phaser.player.body.velocity.y = -jumpingSpeed;
+    }
 }
 
-function handleLevel(phaser) {
-    // Create 3 groups that will contain our objects
-    phaser.walls = phaser.add.group();
-    phaser.enemies = phaser.add.group();
-    
-    // Design the level. x = wall, o = coin, ! = lava.
-    var level = [
-        "xxxxxxxxxxxxxxxxxxxxxx",
-        "!         !          x",
-        "!                    x",
-        "!                    x",
-        "!                    x",
-        "!         !    x     x",
-        "xxxxxxxxxxxxxxxx!!!!!x",
-    ];
-    
-    for (var row = 0; row < level.length; row++) {
-        for (var block = 0; block < level[row].length; block++) {
-            if (level[row][block] == "x") {
-                var wall = phaser.add.sprite(30+16*block, 30+16*row, "wall");
-                phaser.walls.add(wall);
-                wall.body.immovable = true; 
-            } else if (level[row][block] == "!") {
-                var enemy = phaser.add.sprite(30+16*block, 30+16*row, "enemy");
-                phaser.enemies.add(enemy);
+
+function handleLevel(phaser, game)
+{
+    var levels = {
+        "grass"     :   1,
+        "dirt"      :   10,
+        "stone"     :   15,
+        "bedrock"   :   10,
+    };
+
+    var worldWidth = game.world.bounds.width;
+
+    var localChunkX = phaser.player.x - (worldWidth / 2);
+    var localChunkY = game.world.centerY + 32;
+
+    var blockSize = 16;
+
+    var totalBlocks = Math.ceil((worldWidth / blockSize) * 1) / 1;
+
+    var depth = 0;
+    for (var level in levels) {
+        var rows = levels[level];
+
+        for (var row = 0; row < rows; row++) {
+            for (var block = 0; block < totalBlocks; block++) {
+                var blockX = localChunkX + (block * blockSize);
+                var blockY = localChunkY + (depth * blockSize);
+
+                // Ensure that no block already exists at the X and Y coords
+                if (!getBlockInformation(phaser, blockX, blockY)) {
+                    var sprite = phaser.add.sprite(blockX, blockY, level);
+                    sprite.body.immovable = true;
+                    // Allow blocks to be destroyed if it isn't bedrock
+                    if (level != "bedrock") {
+                        sprite.inputEnabled = true;
+                        sprite.events.onInputDown.add(function (sprite) {
+                            phaser.blocks.remove(sprite);
+                            sprite.destroy();
+                        }, this);
+                    }
+                    phaser.blocks.add(sprite);
+                }
             }
+
+            depth++;
         }
     }
 }
 
-function handleCollision(phaser, game) {
-    // Make the player and the walls collide
-    game.physics.arcade.collide(phaser.player, phaser.walls);
-    
-    // Call the 'restart' function when the player touches the enemy
-    game.physics.arcade.overlap(phaser.player, phaser.enemies, phaser.restart, null, phaser);
+
+function handleCollision(phaser, game)
+{
+    game.physics.arcade.collide(phaser.player, phaser.blocks);
+}
+
+
+/**
+ * Check if a block sprite exists at a x and y placement.
+ */
+function getBlockInformation(phaser, x, y)
+{
+    var data = false;
+    for (var i = 0; i < phaser.blocks.length; i++) {
+        var element = phaser.blocks.getAt(i).body;
+        if (element.x != x || element.y != y) {
+            continue;
+        }
+        data = true;
+    }
+
+    return data;
 }
