92a78755987100684f7ae7f611df31cce7f30ae2
tomcphr <tchadwick@regatta.com>
2018-01-09 21:24:06 +0000

    Added a day and night cycle!

--- js/game.js
+++ js/game.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-    var game = new Phaser.Game(800, 480, Phaser.AUTO, "playArea");
+    var game = new Phaser.Game(800, 480, Phaser.WEBGL, "playArea");
 
     game.state.add("play", play);
 
